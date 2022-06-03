8f18050b72b0a40ccd2e7b7ff13d9913313cb191
Al-tsao <al.tsao.yw@gmail.com>
2022-06-03 11:53:12 +0200

    revise

--- BS_PTT_NBA.py
+++ BS_PTT_NBA.py
@@ -4,4 +4,10 @@ import time
 
 today = time.strftime('%m/%d').lstrip('0') #利用time模組取的時間字串
 
-print(today)
+def pttNBA(url):
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        print('URL發生錯誤' + url)
+        return
+    
+    soup = BeautifulSoup(resp.text, 'html5lib')
