61d3dbb5f74f5145dc339d3de17934d7423b9b1a
Al-tsao <al.tsao.yw@gmail.com>
2022-06-05 14:01:25 +0200

    revise

--- BS_PTT_NBA.py
+++ BS_PTT_NBA.py
@@ -12,3 +12,11 @@ def pttNBA(url):
     
     soup = BeautifulSoup(resp.text, 'html5lib')
     paging = soup.find('div', 'btn-group btn-group-paging').find_all('a')[1]['href']
+
+    articles = []
+    rents = soup.find_all('div', 'r-ent')
+    for rent in rents:
+        title = rent.find('div', 'title').text.strip()
+        count = rent.find('div', 'nrec').text.strip()
+        date = rent.find('div', 'meta').find('div', 'date').text.strip()
+        article = '%s %s:%s' % (date, count, title)
