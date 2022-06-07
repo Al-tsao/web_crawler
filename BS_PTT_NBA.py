a327a3d25111279ebd1a37e8fa14b904ac0c6573
Al-tsao <al.tsao.yw@gmail.com>
2022-06-07 09:59:39 +0200

    revise

--- BS_PTT_NBA.py
+++ BS_PTT_NBA.py
@@ -10,8 +10,8 @@ def pttNBA(url):
         print('URL發生錯誤' + url)
         return
     
-    soup = BeautifulSoup(resp.text, 'html5lib')
-    paging = soup.find('div', 'btn-group btn-group-paging').find_all('a')[1]['href']
+    soup = BeautifulSoup(resp.text, 'html5lib') #利用html5lib解析文檔
+    paging = soup.find('div', 'btn-group btn-group-paging').find_all('a')[1]['href'] #使用[]獲得屬性內容，使用.text獲得內文
 
     articles = []
     rents = soup.find_all('div', 'r-ent')
@@ -28,11 +28,14 @@ def pttNBA(url):
             if today == date and count == '爆':
                 articles.append(article)
 
-    if len(articles) != 0:
-        for article in articles:
-            print(article)
-        pttNBA('https://www.ptt.cc' + paging)
-    else:
-        return
+    for article in articles:
+        print(article)
+
+    # if len(articles) != 0:
+    #     for article in articles:
+    #         print(article)
+    #     pttNBA('https://www.ptt.cc' + paging)
+    # else:
+    #     return
 
 pttNBA('https://www.ptt.cc/bbs/NBA/index.html')
