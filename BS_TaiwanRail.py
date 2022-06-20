35fa125d15799bdce1de01eb8609dae7ad13cd7d
Al-tsao <al.tsao.yw@gmail.com>
2022-06-20 18:08:10 +0200

    revise

--- BS_TaiwanRail.py
+++ BS_TaiwanRail.py
@@ -4,3 +4,14 @@ import time
 
 url = 'https://tip.railway.gov.tw/tra-tip-web/tip'
 staDic = {}
+today = time.strftime('%Y/%m/%d')
+sTime = '06:00'
+eTime = '12:00'
+
+def getTrip():
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        print('URL發生錯誤' + url)
+        return
+
+    soup = BeautifulSoup(rep.text, 'html5lib')
