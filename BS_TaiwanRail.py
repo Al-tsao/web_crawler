06eef5eb9772885fd2b41a7d274b84466469cdd8
Al-tsao <al.tsao.yw@gmail.com>
2022-06-22 21:37:15 +0200

    revise

--- BS_TaiwanRail.py
+++ BS_TaiwanRail.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import datetime
 
 url = 'https://tip.railway.gov.tw/tra-tip-web/tip'
 staDic = {}
-today = time.strftime('%Y/%m/%d')
+today = (datetime.datetime.now()+datetime.timedelta(days=1)).strftime('%Y/%m/%d')
 sTime = '06:00'
 eTime = '12:00'
 
@@ -15,12 +16,26 @@ def getTrip():
         return
 
     soup = BeautifulSoup(resp.text, 'html5lib')
+
     stations = soup.find(id = 'cityHot').ul.find_all('li')
     for station in stations:
         stationName = station.button.text
         stationId = station.button['title']
         staDic[stationName] = stationId
 
-    print(staDic)
+    csrf = soup.find(id = 'queryForm').find('input',{'name':'_csrf'})['value']
+
+    formData = {
+        'trainTypeList':'ALL',
+        'transfer':'ONE',
+        'startOrEndTime':'true',
+        'startStation':staDic['臺北'],
+        'endStation':staDic['新竹'],
+        'rideDate':today,
+        'startTime':sTime,
+        'endTime':eTime, 
+        '_csrf':csrf
+    }
+
 
 getTrip()
