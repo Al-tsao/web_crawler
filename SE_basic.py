6cca1da1a120a996db4a419412593d581a6aaa59
Al-tsao <al.tsao.yw@gmail.com>
2022-07-01 09:06:53 +0200

    revise

--- SE_basic.py
+++ SE_basic.py
@@ -6,7 +6,9 @@ from selenium import webdriver
 import soupsieve
 
 try:
-    chrome = webdriver.Chrome(executable_path='chromedriver.exe')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    chrome = webdriver.Chrome(options=options, executable_path='chromedriver.exe')
     chrome.set_page_load_timeout(10)
     chrome.get('https://code-gym.github.io/spider_demo/')
     soup = BeautifulSoup(chrome.page_source, 'html5lib')
