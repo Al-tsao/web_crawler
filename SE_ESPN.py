f342e7f411c0aa8dd452bc4e8c9b3373a727aa07
Al-tsao <al.tsao.yw@gmail.com>
2022-07-05 13:45:30 +0200

    reviese

---
+++ SE_ESPN.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import random
+
+url = 'https://insider.espn.com/nba/hollinger/statistics/_/page/1'
+
+try:
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    chrome = webdriver.Chrome(options=options, executable_path='chromedriver.exe')
+    chrome.set_page_load_timeout(10)
+finally:
