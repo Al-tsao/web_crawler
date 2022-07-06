e00ddadda2d01ac80b3f8dedb4ae297ddc6dcbc7
Al-tsao <al.tsao.yw@gmail.com>
2022-07-06 07:40:20 +0200

    revise

--- SE_ESPN.py
+++ SE_ESPN.py
@@ -10,4 +10,7 @@ try:
     options.add_argument('--headless')
     chrome = webdriver.Chrome(options=options, executable_path='chromedriver.exe')
     chrome.set_page_load_timeout(10)
+    for i in range (i,9):
+        _url = url + str(i)
+        print(_url)
 finally:
