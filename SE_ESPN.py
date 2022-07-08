4151e170c37fd7f0feb33cba2fc6d17e43c9335a
Al-tsao <al.tsao.yw@gmail.com>
2022-07-08 14:59:54 +0200

    revise

--- SE_ESPN.py
+++ SE_ESPN.py
@@ -15,4 +15,8 @@ try:
         print(_url)
         chrome.get(_url)
         soup = BeautifulSoup(chrome.page_source, 'html5lib')
+        trs = soup.find('tbody').find_all('tr')
+        for tr in trs:
+            tds = [td for td in tr.children]
+            rk = tds[0].text.strip()
 finally:
